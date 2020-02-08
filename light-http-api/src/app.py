8b4969f1b61d7c9dcaa82f2efeeed216c4a429ba
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2020-02-08 16:56:45 +0200

    Adding demo re route.

--- light-http-api/src/app.py
+++ light-http-api/src/app.py
@@ -1,15 +1,20 @@
 import uhttp
 import middleware
 import logger
+import ure
 
 def pong(w, req):
     w.write("PONG")
+
+def get_pin(w, req, match):
+    w.write("PIN: %s" % match.group(1))
 
 def main():
     logger.setup()
 
     router = middleware.create_router([
-        ("/ping", pong)
+        ("/ping", pong),
+        (ure.compile(r"^/pins/(\d+)$"), get_pin)
     ])
 
     default_handler = middleware.not_found(None)
