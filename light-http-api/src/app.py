a2cadbb81e76aefe40e56072ef164e985f6a15b8
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2020-02-18 23:24:56 +0200

    Pending API for demo purposes.

--- light-http-api/src/app.py
+++ light-http-api/src/app.py
@@ -2,9 +2,20 @@ import uhttp
 import middleware
 import logger
 import ure
+from machine import UART
+
+uart = UART(0)
 
 def pong(w, req):
     w.write("PONG")
+
+def led_on(w, req):
+    uart.write("AT+LED=ON\n")
+    w.write("LED_ON")
+
+def led_off(w, req):
+    uart.write("AT+LED=OFF\n")
+    w.write("LED_OFF")
 
 def get_pin(w, req, match):
     w.write("PIN: %s" % match.group(1))
@@ -12,6 +23,8 @@ def get_pin(w, req, match):
 def create_server(*, port=8080):
     router = middleware.create_router([
         ("/ping", pong),
+        ("/led/on", led_on),
+        ("/led/off", led_off),
         (ure.compile(r"^/pins/(\d+)$"), get_pin)
     ])
 
