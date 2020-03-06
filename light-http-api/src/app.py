b62162d39fc8322c44e554d64b49f292740744fc
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2020-03-07 00:44:20 +0200

    Temporary disabling testing stuff.

--- light-http-api/src/app.py
+++ light-http-api/src/app.py
@@ -2,20 +2,20 @@ import uhttp
 import middleware
 import logger
 import ure
-from machine import UART
+# from machine import UART
 
-uart = UART(0)
+# uart = UART(0)
 
 def pong(w, req):
     w.write("PONG")
 
-def led_on(w, req):
-    uart.write("AT+LED=ON\n")
-    w.write("LED_ON")
+# def led_on(w, req):
+#     uart.write("AT+LED=ON\n")
+#     w.write("LED_ON")
 
-def led_off(w, req):
-    uart.write("AT+LED=OFF\n")
-    w.write("LED_OFF")
+# def led_off(w, req):
+#     uart.write("AT+LED=OFF\n")
+#     w.write("LED_OFF")
 
 def get_pin(w, req, match):
     w.write("PIN: %s" % match.group(1))
@@ -23,16 +23,16 @@ def get_pin(w, req, match):
 def create_server(*, port=8080):
     router = middleware.create_router([
         ("/ping", pong),
-        ("/led/on", led_on),
-        ("/led/off", led_off),
+        # ("/led/on", led_on),
+        # ("/led/off", led_off),
         (ure.compile(r"^/pins/(\d+)$"), get_pin)
     ])
 
     default_handler = middleware.not_found(None)
     server = uhttp.HTTPServer(
         handler=middleware.use(default_handler,
-            middleware.recover,
             middleware.trace,
+            middleware.recover,
             router,
         ),
         port=port,
