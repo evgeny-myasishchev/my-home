be0b7de96bff7d31a2aae0518236218fbe045eff
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2020-03-22 15:47:23 +0200

    Adding simple header.

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -1,9 +1,14 @@
-import {Icon, Button} from 'react-native-elements';
+import {Button, Header, Icon} from 'react-native-elements';
 import React from 'react';
 
 function App() {
   return (
     <>
+      <Header
+        leftComponent={{icon: 'menu', color: '#fff'}}
+        centerComponent={{text: 'MY TITLE', style: {color: '#fff'}}}
+        rightComponent={{icon: 'home', color: '#fff'}}
+      />
       <Button title="Hey!" />
       <Icon name="rowing" />
     </>
