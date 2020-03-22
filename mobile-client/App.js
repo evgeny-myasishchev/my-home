1a1819003e9de655e9915fe239d8669044aea685
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2020-03-22 15:08:13 +0200

    Integrate react-native-elements ui lib.

--- mobile-client/App.js
+++ mobile-client/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Icon, Button, ThemeProvider} from 'react-native-elements';
 
 function App() {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <ThemeProvider>
+      <Button title="Hey!" />
+      <Icon name="rowing" />
+    </ThemeProvider>
   );
 }
 
