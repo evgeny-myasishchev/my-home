14b9a2766c57950bdd4a1c355b3f395f0c0af192
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2020-03-23 23:11:46 +0200

    Most recent navigation package.

--- mobile-client/index.js
+++ mobile-client/index.js
@@ -2,7 +2,6 @@
 import 'react-native-gesture-handler';
 
 import {AppRegistry} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
 import {ThemeProvider} from 'react-native-elements';
 import React from 'react';
 
@@ -11,11 +10,9 @@ import App from './App';
 
 function Main() {
   return (
-    <NavigationContainer>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </NavigationContainer>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   );
 }
 
