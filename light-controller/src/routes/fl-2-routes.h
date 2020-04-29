a0871d7b4ba48b47dc87a78636bacb29a03dc4e7
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2020-04-29 15:40:16 +0300

    Actualizing fl-2 routes.

--- light-controller/src/routes/fl-2-routes.h
+++ light-controller/src/routes/fl-2-routes.h
@@ -9,63 +9,45 @@ using namespace v2;
 
 ArrayPtr<Switch *> createRoutes()
 {
-    const byte switchesOffset = RELAY_BOARDS * 8;
+    const byte relaysCount = RELAY_BOARDS * 8;
     const byte switchesCount = INPUT_BOARDS * 8;
 
     Switch * *routesArray = new Switch*[switchesCount];
 
     // First two switch boards are controlling corresponding pins
     // for now
-    for (byte switchNr = 0; switchNr < 16; switchNr++)
-    {
-        Switch* route = (new Switch())
-            ->withSwitchAddress(switchesOffset + switchNr)
-            ->withTargetAddresses(1, new byte[1]{switchNr});
-        routesArray[switchNr] = route;
-    }
+    // for (byte switchNr = 0; switchNr < 15; switchNr++)
+    // {
+    //     Switch* route = (new Switch())
+    //         ->withSwitchAddress(relaysCount + switchNr)
+    //         ->withTargetAddresses(1, new byte[1]{switchNr});
+    //     routesArray[switchNr] = route;
+    // }
 
-    // Last board is controlling all
-    byte* allRelays = new byte[RELAY_BOARDS * 8];
-    for (byte relayNum = 0; relayNum < RELAY_BOARDS * 8; relayNum++)
+    byte routeNumber = 0;
+    Switch* route;
+
+    // Kids bedroom (green) - room light
+    route = (new Switch())
+        ->withSwitchAddress(relaysCount + 0)
+        ->withTargetAddresses(1, new byte[1]{5});
+    routesArray[routeNumber++] = route;
+
+    // Kids bedroom (green) - hall
+    route = (new Switch())
+        ->withSwitchAddress(relaysCount + 1)
+        ->withTargetAddresses(1, new byte[1]{0});
+    routesArray[routeNumber++] = route;
+
+    byte* allRelays = new byte[relaysCount];
+    for (byte relayNum = 0; relayNum < relaysCount; relayNum++)
     {
         allRelays[relayNum] = relayNum;
     }
+    routesArray[routeNumber++] = (new Switch())
+        ->withSwitchType(SwitchType::Toggle)
+        ->withSwitchAddress(relaysCount + 16)
+        ->withTargetAddresses(relaysCount, allRelays);
 
-    routesArray[16] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 16)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    routesArray[17] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 17)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    routesArray[18] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 18)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    routesArray[19] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 19)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    routesArray[20] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 20)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    routesArray[21] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 21)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    routesArray[22] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 22)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    routesArray[23] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 23)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    routesArray[23] = (new Switch())
-        ->withSwitchAddress(switchesOffset + 23)
-        ->withTargetAddresses(RELAY_BOARDS, allRelays);
-
-    return ArrayPtr<Switch *>(switchesCount, routesArray);
+    return ArrayPtr<Switch *>(routeNumber, routesArray);
 }
