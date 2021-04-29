f2a2e9d59b5898be0d4cac9e54832da4b0ab70a7
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2021-04-29 21:35:15 +0300

    Improve outdoor routes.

--- light-controller/src/routes/outdoor-routes.h
+++ light-controller/src/routes/outdoor-routes.h
@@ -6,6 +6,13 @@ using namespace v2;
 #define RELAY_BOARDS 2
 #define INPUT_BOARDS 2
 
+#define SPOT_ADDR_LAND_NIGHT 0
+#define SPOT_ADDR_LAND_PAVILION 1
+#define SPOT_ADDR_SHED_COM 2
+#define SPOT_ADDR_SHED_NIGHT_1 3
+#define SPOT_ADDR_SHED_RESERVE_1 4
+#define SPOT_ADDR_BARN_COM 5
+
 ArrayPtr<Switch *> createRoutes(SolarSwitch *solarSwitch)
 {
 
@@ -27,14 +34,14 @@ ArrayPtr<Switch *> createRoutes(SolarSwitch *solarSwitch)
     route = (new Switch())
         ->withSwitchAddress(relaysCount + 0)
         ->withSwitchType(SwitchType::Push)
-        ->withTargetAddresses(1, new byte[1]{8});
+        ->withTargetAddresses(1, new byte[1]{SPOT_ADDR_BARN_COM});
     routesArray[routeNumber++] = route;
     
     // shed normal
     route = (new Switch())
         ->withSwitchAddress(relaysCount + 2)
         ->withSwitchType(SwitchType::Push)
-        ->withTargetAddresses(1, new byte[1]{9});
+        ->withTargetAddresses(2, new byte[2]{SPOT_ADDR_SHED_COM, SPOT_ADDR_LAND_PAVILION});
     routesArray[routeNumber++] = route;
 
     // night land + shed
@@ -48,7 +55,7 @@ ArrayPtr<Switch *> createRoutes(SolarSwitch *solarSwitch)
     route = (new Switch())
         ->withSwitchAddress(virtualAddressStart + 0)
         ->withSwitchType(SwitchType::Toggle)
-        ->withTargetAddresses(2, new byte[2]{0, 3});
+        ->withTargetAddresses(2, new byte[2]{SPOT_ADDR_LAND_NIGHT, SPOT_ADDR_SHED_NIGHT_1});
     routesArray[routeNumber++] = route;
 
     return ArrayPtr<Switch *>(routeNumber, routesArray);
